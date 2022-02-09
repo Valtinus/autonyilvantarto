a68d157719bdaeb7e9c77b94c840eb45dadc0657
Bálint Bank <bankbalint@gmail.om>
2022-02-09 09:13:18 +0100

    01

---
+++ src/App.js
@@ -0,0 +1,86 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import _ from "lodash";
+import Form from "./components/NewCar";
+
+const setLocalData = (obj) => {
+  if (typeof obj !== "object") return false;
+  localStorage.setItem("data", JSON.stringify(obj));
+  return getLocalData();
+};
+
+const getLocalData = () => {
+  return JSON.parse(localStorage.getItem("data"));
+};
+
+const getRemoteData = async () => {
+  const result = await fetch("http://localhost:3004/data");
+  return result.json();
+};
+
+const stampToDate = (stamp) => {
+  return new Date(stamp*1000).toLocaleString('hu-HU', { year: 'numeric', month: '2-digit', day: '2-digit'})
+}
+
+function App() {
+  const [state, setState] = useState({ data: [] });
+  const [loading, setLoading] = useState(true);
+  const [form, setForm] = useState(false);
+
+  useEffect(() => {
+    getRemoteData().then((remote) => {
+      setState({
+        ...state,
+        data: 
+          !_.isEqual(getLocalData(), remote)
+          ? setLocalData(remote)
+          : getLocalData(),
+      });
+      setLoading(false);
+    });
+  }, []);
+
+  return (
+    <>
+      <div className="header">
+        <h1>Autónyilvántartás</h1>
+        <button className="new_car" onClick={() => setForm(true)}>Új autó</button>
+      </div>
+      <div className="form">
+        {form === true && <Form setForm={setForm} setState={setState}/>}
+      </div>
+      <div className="container">
+        <table border="1">
+          <thead>
+            <tr>
+              <th>Gyártó</th>
+              <th>Típus</th>
+              <th>Motor (cm3)</th>
+              <th>Szín</th>
+              <th>Kivitel</th>
+              <th>Gyártási időpont</th>
+              <th>Gyártó weboldala</th>
+            </tr>
+          </thead>
+          {loading === true && "Töltés..."}
+          {console.log(state.data)}
+          {state.data.map((item, index) => {
+            return (
+              <tr key={index}>
+                <td>{item.manufacturer}</td>
+                <td>{item.type}</td>
+                <td>{item.ccm}</td>
+                <td>{item.color}</td>
+                <td>{item.design}</td>
+                <td>{stampToDate(item.year)}</td>
+                <td>{item.web}</td>
+              </tr>
+            );
+          })}
+        </table>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/NewCar.jsx
@@ -0,0 +1,178 @@
+import React, { useState } from "react";
+import "./NewCar.css";
+import * as Yup from "yup";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+
+const NewCar = (props) => {
+  const [formValue, setformValue] = useState({
+    manufacturer: "",
+    type: "",
+    ccm: "",
+    color: "",
+    design: "",
+    year: new Date(),
+    web: "",
+  });
+
+  const validationSchema = Yup.object({
+    manufacturer: Yup.string().required(),
+    type: Yup.string().required(),
+    ccm: Yup.number().required(),
+    color: Yup.string(),
+    design: Yup.string(),
+    year: Yup.date()
+      .default(() => new Date())
+      .required(),
+    web: Yup.string().matches(
+      /((https?):\/\/)?(www.)?[a-z0-9]+(\.[a-z]{2,}){1,3}(#?\/?[a-zA-Z0-9#]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?)?$/,
+      "Helytelen url!"
+    ),
+  });
+
+  const initialValues = {
+    manufacturer: "",
+    type: "",
+    ccm: "",
+    color: "",
+    design: "",
+    year: new Date(),
+    web: "",
+  };
+
+  const renderError = (message) => <p className="help is-danger">{message}</p>;
+
+  const onSubmit = (values) => {
+    alert(JSON.stringify(values, null, 2));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const car = {
+      id: Math.floor(Math.random() * 1000000),
+      manufacturer: formValue.manufacturer,
+      type: formValue.type,
+      ccm: formValue.ccm,
+      color: formValue.color,
+      design: formValue.design,
+      year: formValue.year,
+      web: formValue.web,
+    };
+
+    console.log(props.state);
+
+    props.setState({ ...props.state, data: props.state.data.push(car) });
+
+    console.log(props.state);
+  };
+
+  const handleChange = (event) => {
+    setformValue({
+      ...formValue,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  return (
+    <div className="form">
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={async (values, {resetForm}) => {
+            await onSubmit(values)
+            resetForm()
+        }}
+      >
+          <Form>
+        <label htmlFor="manufacturer">Gyártó* </label>
+        <Field
+          type="text"
+          id="manufacturer"
+          name="manufacturer"
+        />
+        <ErrorMessage name="manufacturer" render={renderError} />
+        <br />
+        <label htmlFor="type">Típus* </label>
+        <Field
+          type="text"
+          id="type"
+          name="type"
+        />
+        <ErrorMessage name="type" render={renderError} />
+        <br />
+        <label htmlFor="ccm">Motor hengerűrtartalom* </label>
+        <Field
+          type="number"
+          id="ccm"
+          name="ccm"
+        />
+        <ErrorMessage name="ccm" render={renderError} />
+        (cm3)
+        <br />
+        <label htmlFor="color">Szín </label>
+        <Field
+          type="text"
+          id="color"
+          name="color"
+        />
+        <ErrorMessage name="color" render={renderError} />
+        <br />
+        <label htmlFor="design">Kivitel </label>
+        <Field
+          type="text"
+          id="design"
+          name="design"
+        />
+        <ErrorMessage name="design" render={renderError} />
+        <br />
+        <label htmlFor="date">Gyártási időpont* </label>
+        <Field
+          type="date"
+          id="date"
+          name="date"
+        />
+        <ErrorMessage name="date" render={renderError} />
+        <br />
+        <label htmlFor="web">Gyártó weboldala </label>
+        <Field
+          type="text"
+          id="web"
+          name="web"
+        />
+        <ErrorMessage name="web" render={renderError} />
+        <div className="buttons">
+          <button type="submit">Új autó</button>
+          <button
+            type="reset"
+            onClick={() =>
+              setformValue({
+                ...formValue,
+                manufacturer: "",
+                type: "",
+                ccm: "",
+                color: "",
+                design: "",
+                year: "",
+                web: "",
+              })
+            }
+          >
+            Törlés
+          </button>
+          <button onClick={() => props.setForm(false)}>Mégse</button>
+        </div>
+        </Form>
+      </Formik>
+    </div>
+  );
+};
+
+export default NewCar;
+
+// manufacturer: "Ford",
+// type: "FORD FIESTA CONNECTED 1.1L PFI3",
+// ccm: 1100,
+// color: "MAGNETIC",
+// design: "Kupé",
+// year: 4322342343,
+// web:
